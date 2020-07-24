0f945946806429bde9cce508e4117e100c2c5df4
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-07-23 21:17:06 -0700

    Created-React-App

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <section>
+          <div>
+            <h1>Hi, I am Jonathan Lane</h1>
+            <p>This is a sentence</p>
+            <a href="#">Link</a>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/pages/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Layout from '../../components/Layout/Layout'
+
+class App extends Component {
+  /*--- State ---*/
+  /*--- Handle Methods ---*/
+  /*--- Lifecycle Methods ---*/
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">MY APP</header>
+        <Layout />
+      </div>
+    );
+  }
+}
+export default App;
