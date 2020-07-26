ea7061897197d728c7315d64c09afa2d965b1b3b
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-07-25 18:04:04 -0700

    Creating function for theme changes

--- public/script.js
+++
@@ -1,7 +0,0 @@
-let themeDots = document.getElementsByClassName('theme-dot');
-
-for(var i = 0; themeDots.length > i; i++) {
-  themeDots[i].addEventListener('click', function() {
-    console.log('Option clicked')
-  })
-}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
-import "./themes/purple.css";
 import App from "./pages/App/App";
 import * as serviceWorker from "./serviceWorker";
 
