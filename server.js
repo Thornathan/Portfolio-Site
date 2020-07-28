78e53ca13ecdd3ccf5bb90a23da943441066e503
Jonathan <Jonathan@Admins-MacBook-Pro.local>
2020-07-28 01:18:45 -0700

    Setting up deployment

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
